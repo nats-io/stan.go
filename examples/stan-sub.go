8f7adc640b6c99a75504f4a03a0e1370e1084ba1
Larry McQueary <larry@apcera.com>
2016-03-27 22:33:55 -0600

    Added qgroup option

--- examples/stan-sub.go
+++ examples/stan-sub.go
@@ -11,7 +11,6 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/nats-io/nuid"
 	. "github.com/nats-io/stan"
 	"github.com/nats-io/stan/pb"
 )
@@ -25,12 +24,13 @@ Options:
 	-id,--clientid <client ID>      STAN client ID
 
 Subscription Options:
-	-seq <seqno>                    Start at seqno
-	-all                            Deliver all available messages
-	-last                           Deliver starting with last published message
-	-since <duration>               Deliver messages in last interval (e.g. 1s, 1hr)
-	          (for more information: https://golang.org/pkg/time/#ParseDuration)
-	-durable <name>                 Durable subscriber name
+	--qgroup <name>                 Queue group
+	--seq <seqno>                   Start at seqno
+	--all                           Deliver all available messages
+	--last                          Deliver starting with last published message
+	--since <duration>              Deliver messages in last interval (e.g. 1s, 1hr)
+	         (for more information: https://golang.org/pkg/time/#ParseDuration)
+	--durable <name>                Durable subscriber name
 `
 
 // NOTE: Use tls scheme for TLS, e.g. stan-sub -s tls://demo.nats.io:4443 foo
@@ -53,15 +53,16 @@ func main() {
 	var deliverAll bool
 	var deliverLast bool
 	var durable string
+	var qgroup string
 
-	defaultID := fmt.Sprintf("client.%s", nuid.Next())
+	//	defaultID := fmt.Sprintf("client.%s", nuid.Next())
 
 	flag.StringVar(&opts.NatsURL, "s", DefaultNatsURL, "The nats server URLs (separated by comma)")
 	flag.StringVar(&opts.NatsURL, "server", DefaultNatsURL, "The nats server URLs (separated by comma)")
 	flag.StringVar(&clusterID, "c", "test-cluster", "The STAN cluster ID")
 	flag.StringVar(&clusterID, "cluster", "test-cluster", "The STAN cluster ID")
-	flag.StringVar(&clientID, "id", defaultID, "The STAN client ID to connect with")
-	flag.StringVar(&clientID, "clientid", defaultID, "The STAN client ID to connect with")
+	flag.StringVar(&clientID, "id", "", "The STAN client ID to connect with")
+	flag.StringVar(&clientID, "clientid", "", "The STAN client ID to connect with")
 	flag.BoolVar(&showTime, "t", false, "Display timestamps")
 	// Subscription options
 	flag.Uint64Var(&startSeq, "seq", 0, "Start at sequence no.")
@@ -69,6 +70,7 @@ func main() {
 	flag.BoolVar(&deliverLast, "last", false, "Start with last value")
 	flag.StringVar(&startDelta, "since", "", "Deliver messages since specified time offset")
 	flag.StringVar(&durable, "durable", "", "Durable subscriber name")
+	flag.StringVar(&durable, "qgroup", "", "Queue group name")
 
 	log.SetFlags(0)
 	flag.Usage = usage
@@ -81,13 +83,9 @@ func main() {
 
 	sc, err := Connect(clusterID, clientID)
 	if err != nil {
-		log.Fatal(err)
-	}
-	defer sc.Close()
-
-	if err != nil {
 		log.Fatalf("Can't connect: %v\n", err)
 	}
+	log.Printf("Connected to %s clusterID: [%s] clientID: [%s]\n", opts.NatsURL, clusterID, clientID)
 
 	subj, i := args[0], 0
 
@@ -114,14 +112,14 @@ func main() {
 		startOpt = StartAtTimeDelta(ago)
 	}
 
-	//	sub, err := sc.Subscribe(subj, mcb, startOpt)
-	sub, err := sc.Subscribe(subj, mcb, startOpt, DurableName(durable))
+	sub, err := sc.QueueSubscribe(subj, qgroup, mcb, startOpt, DurableName(durable))
 	if err != nil {
 		sc.Close()
 		log.Fatal(err)
 	}
 
-	log.Printf("Listening on [%s]\n", subj)
+	log.Printf("Listening on [%s], clientID=[%s], qgroup=[%s] durable=[%s]\n", subj, clientID, qgroup, durable)
+
 	if showTime {
 		log.SetFlags(log.LstdFlags)
 	}
