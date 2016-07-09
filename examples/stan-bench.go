e43be79c706d5f9a247a31e9096efc3ec5b9edd8
Alberto Ricart <alberto.ricart@apcera.com>
2016-07-09 08:31:13 -0500

    Updated stan-bench to use nats bench library.

--- examples/stan-bench.go
+++ examples/stan-bench.go
@@ -1,20 +1,19 @@
 // Copyright 2015 Apcera Inc. All rights reserved.
-// +build ignore
 
 package main
 
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"math"
-	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/nats-io/go-nats-streaming"
 	"github.com/nats-io/nats"
+	"github.com/nats-io/nats/bench"
 )
 
 // Some sane defaults
@@ -30,11 +29,10 @@ const (
 )
 
 func usage() {
-	log.Fatalf("Usage: nats-bench [-s server (%s)] [--tls] [-id CLIENT_ID] [-np NUM_PUBLISHERS] [-ns NUM_SUBSCRIBERS] [-n NUM_MSGS] [-ms MESSAGE_SIZE] [-io] [-a] <subject>\n", nats.DefaultURL)
+	log.Fatalf("Usage: nats-bench [-s server (%s)] [--tls] [-id CLIENT_ID] [-np NUM_PUBLISHERS] [-ns NUM_SUBSCRIBERS] [-n NUM_MSGS] [-ms MESSAGE_SIZE] [-csv csvfile] [-mpa MAX_NUMBER_OF_PUBLISHED_ACKS_INFLIGHT] [-io] [-a] <subject>\n", nats.DefaultURL)
 }
 
-var subStatChan chan *stats
-var pubStatChan chan *stats
+var benchmark *bench.Benchmark
 
 func main() {
 	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
@@ -47,6 +45,7 @@ func main() {
 	var ignoreOld = flag.Bool("io", DefaultIgnoreOld, "Subscribers Ignore Old Messages")
 	var maxPubAcks = flag.Int("mpa", DefaultMaxPubAcksInflight, "Max number of published acks in flight")
 	var clientID = flag.String("id", DefaultClientID, "Benchmark process base client ID.")
+	var csvFile = flag.String("csv", "", "Save bench data to csv file")
 
 	log.SetFlags(0)
 	flag.Usage = usage
@@ -65,72 +64,42 @@ func main() {
 	}
 	opts.Secure = *tls
 
+	benchmark = bench.NewBenchmark("NATS Streaming", *numSubs, *numPubs)
+
 	var startwg sync.WaitGroup
 	var donewg sync.WaitGroup
-
-	subStatChan = make(chan *stats, *numSubs)
-	pubStatChan = make(chan *stats, *numPubs)
 
 	donewg.Add(*numPubs + *numSubs)
 
 	// Run Subscribers first
 	startwg.Add(*numSubs)
+	subCounts := bench.MsgsPerClient(*numMsgs, *numSubs)
 	for i := 0; i < *numSubs; i++ {
 		subID := fmt.Sprintf("%s-sub-%d", *clientID, i)
-		go runSubscriber(&startwg, &donewg, opts, *numMsgs, *ignoreOld, subID)
+		go runSubscriber(&startwg, &donewg, opts, subCounts[i], *messageSize, *ignoreOld, subID)
 	}
 	startwg.Wait()
 
 	// Now Publishers
 	startwg.Add(*numPubs)
+	pubCounts := bench.MsgsPerClient(*numMsgs, *numPubs)
 	for i := 0; i < *numPubs; i++ {
 		pubID := fmt.Sprintf("%s-pub-%d", *clientID, i)
-		go runPublisher(&startwg, &donewg, opts, *numMsgs, *messageSize, *async, pubID, *maxPubAcks)
+		go runPublisher(&startwg, &donewg, opts, pubCounts[i], *messageSize, *async, pubID, *maxPubAcks)
 	}
 
-	log.Printf("Starting benchmark [msgs=%d, pubs=%d, subs=%d]\n", *numMsgs, *numPubs, *numSubs)
+	log.Printf("Starting benchmark [msgs=%d, msgsize=%d, pubs=%d, subs=%d]\n", *numMsgs, *messageSize, *numPubs, *numSubs)
 
 	startwg.Wait()
-	start := time.Now()
 	donewg.Wait()
-	interval := time.Since(start)
-	delta := interval.Seconds()
 
-	close(pubStatChan)
-	pubStats := newStatSums()
-	for s := range pubStatChan {
-		pubStats.addStat(s)
-	}
+	benchmark.Close()
+	fmt.Print(benchmark.Report())
 
-	close(subStatChan)
-	subStats := newStatSums()
-	for s := range subStatChan {
-		subStats.addStat(s)
-	}
-
-	msgCount := pubStats.msgCount + subStats.msgCount
-	msgThroughput := commaFormat(int64(float64(msgCount) / delta))
-
-	fmt.Printf("STAN (Publishers/Subscribers) throughput is %s msgs/sec (%d msgs in %v)\n", msgThroughput, msgCount, interval)
-
-	if len(pubStats.clients) > 0 {
-		fmt.Printf("Publisher Stats (%d) %v\n", *numPubs, pubStats)
-		if len(pubStats.clients) > 1 {
-			for i, stat := range pubStats.clients {
-				fmt.Printf("  [%d] %v\n", i+1, stat)
-			}
-			fmt.Printf("  %s\n", pubStats.minMaxAverage())
-		}
-	}
-
-	if len(subStats.clients) > 0 {
-		fmt.Printf("Subscriber Stats (%d) %v\n", *numSubs, subStats)
-		if len(subStats.clients) > 1 {
-			for i, stat := range subStats.clients {
-				fmt.Printf("  [%d] %v\n", i+1, stat)
-			}
-			fmt.Printf("  %s\n", subStats.minMaxAverage())
-		}
+	if len(*csvFile) > 0 {
+		csv := benchmark.CSV()
+		ioutil.WriteFile(*csvFile, []byte(csv), 0644)
+		fmt.Printf("Saved metric data in csv file %s\n", *csvFile)
 	}
 }
 
@@ -141,13 +110,9 @@ func setMaxPubAcksInflight(val int) stan.Option {
 	}
 }
 
-func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int, messageSize int, async bool, pubID string, maxPubAcksInFlight int) {
+func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int, msgSize int, async bool, pubID string, maxPubAcksInFlight int) {
 
-	nc, err := opts.Connect()
-	if err != nil {
-		log.Fatalf("Can't connect: %v\n", err)
-	}
-	snc, err := stan.Connect("test-cluster", pubID, setMaxPubAcksInflight(maxPubAcksInFlight), stan.NatsConn(nc))
+	snc, err := stan.Connect("test-cluster", pubID, setMaxPubAcksInflight(maxPubAcksInFlight))
 	if err != nil {
 		log.Fatalf("Publisher %s can't connect: %v\n", pubID, err)
 	}
@@ -158,8 +123,8 @@ func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs in
 
 	subj := args[0]
 	var msg []byte
-	if(messageSize > 0) {
-		msg = make([]byte, messageSize)
+	if msgSize > 0 {
+		msg = make([]byte, msgSize)
 	}
 	published := 0
 	start := time.Now()
@@ -189,19 +154,13 @@ func runPublisher(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs in
 		}
 	}
 
-	pubStatChan <- newStats(start, time.Now(), nc, true)
-
+	benchmark.AddPubSample(bench.NewSample(numMsgs, msgSize, start, time.Now(), snc.NatsConn()))
 	snc.Close()
-	nc.Close()
 	donewg.Done()
 }
 
-func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int, ignoreOld bool, subID string) {
-	nc, err := opts.Connect()
-	if err != nil {
-		log.Fatalf("Can't connect: %v\n", err)
-	}
-	snc, err := stan.Connect("test-cluster", subID, stan.NatsConn(nc))
+func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs int, msgSize int, ignoreOld bool, subID string) {
+	snc, err := stan.Connect("test-cluster", subID)
 	if err != nil {
 		log.Fatalf("Subscriber %s can't connect: %v\n", subID, err)
 	}
@@ -227,174 +186,7 @@ func runSubscriber(startwg, donewg *sync.WaitGroup, opts nats.Options, numMsgs i
 	startwg.Done()
 
 	<-ch
-	subStatChan <- newStats(start, time.Now(), nc, false)
+	benchmark.AddSubSample(bench.NewSample(numMsgs, msgSize, start, time.Now(), snc.NatsConn()))
 	snc.Close()
-	nc.Close()
 	donewg.Done()
-}
-
-type stats struct {
-	msgCount uint64
-	ioBytes  uint64
-	start    time.Time
-	end      time.Time
-}
-
-func (s *stats) throughput() float64 {
-	return float64(s.ioBytes) / s.duration().Seconds()
-}
-
-func (s *stats) rate() int64 {
-	return int64(float64(s.msgCount) / s.duration().Seconds())
-}
-
-func newStats(start, end time.Time, nc *nats.Conn, isPub bool) *stats {
-	s := stats{start: start, end: end}
-	if isPub {
-		s.msgCount = nc.OutMsgs
-		s.ioBytes = nc.OutBytes
-	} else {
-		s.msgCount = nc.InMsgs
-		s.ioBytes = nc.InBytes
-	}
-	return &s
-}
-
-func (s *stats) String() string {
-	rate := commaFormat(s.rate())
-	messages := commaFormat(int64(s.msgCount))
-	throughput := humanBytes(s.throughput(), false)
-	return fmt.Sprintf("%s msgs/sec | %s msgs in %v | %s/sec", rate, messages, s.duration(), throughput)
-}
-
-func (s *stats) duration() time.Duration {
-	return s.end.Sub(s.start)
-}
-
-func (s *stats) Seconds() float64 {
-	return s.duration().Seconds()
-}
-
-func (s *statSums) minMaxAverage() string {
-	return fmt.Sprintf("min %s | avg %s | max %s | stddev %s msgs\n", commaFormat(s.minRate()), commaFormat(s.avgRate()), commaFormat(s.maxRate()), commaFormat(int64(s.stddev())))
-}
-
-func (s *statSums) minRate() int64 {
-	m := int64(0)
-	for i, c := range s.clients {
-		if i == 0 {
-			m = c.rate()
-		}
-		m = min(m, c.rate())
-	}
-	return m
-}
-
-func (s *statSums) maxRate() int64 {
-	m := int64(0)
-	for i, c := range s.clients {
-		if i == 0 {
-			m = c.rate()
-		}
-		m = max(m, c.rate())
-	}
-	return m
-}
-
-func (s *statSums) avgRate() int64 {
-	sum := uint64(0)
-	for _, c := range s.clients {
-		sum += uint64(c.rate())
-	}
-	return int64(sum / uint64(len(s.clients)))
-}
-
-func (s *statSums) stddev() float64 {
-	avg := float64(s.avgRate())
-	sum := float64(0)
-	for _, c := range s.clients {
-		sum += math.Pow(float64(c.rate())-avg, 2)
-	}
-	variance := sum / float64(len(s.clients))
-	return math.Sqrt(variance)
-}
-
-type statSums struct {
-	stats
-	clients []*stats
-}
-
-func newStatSums() *statSums {
-	s := new(statSums)
-	s.clients = make([]*stats, 0, 0)
-	return s
-}
-
-func (s *statSums) addStat(e *stats) {
-	s.clients = append(s.clients, e)
-
-	if len(s.clients) == 1 {
-		s.start = e.start
-		s.end = e.end
-	}
-	s.ioBytes += e.ioBytes
-	s.msgCount += e.msgCount
-
-	if e.start.Before(s.start) {
-		s.start = e.start
-	}
-
-	if e.end.After(s.end) {
-		s.end = e.end
-	}
-}
-
-func commaFormat(n int64) string {
-	in := strconv.FormatInt(n, 10)
-	out := make([]byte, len(in)+(len(in)-2+int(in[0]/'0'))/3)
-	if in[0] == '-' {
-		in, out[0] = in[1:], '-'
-	}
-	for i, j, k := len(in)-1, len(out)-1, 0; ; i, j = i-1, j-1 {
-		out[j] = in[i]
-		if i == 0 {
-			return string(out)
-		}
-		if k++; k == 3 {
-			j, k = j-1, 0
-			out[j] = ','
-		}
-	}
-}
-
-func humanBytes(bytes float64, si bool) string {
-	var base = 1024
-	pre := [...]string{"K", "M", "G", "T", "P", "E"}
-	var post = "B"
-	if si {
-		base = 1000
-		pre = [...]string{"k", "M", "G", "T", "P", "E"}
-		post = "iB"
-	}
-	if bytes < float64(base) {
-		return fmt.Sprintf("%.2f B", bytes)
-	}
-	exp := int(math.Log(bytes) / math.Log(float64(base)))
-	index := exp - 1
-	units := pre[index] + post
-	return fmt.Sprintf("%.2f %s", bytes/math.Pow(float64(base), float64(exp)), units)
-}
-
-func min(x, y int64) int64 {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func max(x, y int64) int64 {
-	if x > y {
-		return x
-	}
-	return y
 }
